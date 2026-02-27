4124026420932bb7b2e71e8ab4357b25c79bd774
DucHungiOS <106652935+DucHungiOS@users.noreply.github.com>
2026-02-27 18:46:23 +0700

    Fix bug

--- PanModal/Controller/PanModalPresentationController.swift
+++ PanModal/Controller/PanModalPresentationController.swift
@@ -869,7 +869,7 @@ private extension PanModalPresentationController {
         // Set path as a mask to display optional drag indicator view & rounded corners
         let mask = CAShapeLayer()
         mask.path = path.cgPath
-        view.layer.mask = mask
+        view.layer.addSublayer(mask)
 
         if #available(iOS 19, *) {
             if let oldMask = view.layer.sublayers?.first(where: { $0.name == "panModalMask" }) {
